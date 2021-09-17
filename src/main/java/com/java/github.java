e607005933e1bc6b6595eb595024977c9e7e29ce
86198 <3913168940@qq.com>
2021-09-17 13:14:43 +0800

    goneng2

--- src/main/java/com/java/github.java
+++ src/main/java/com/java/github.java
@@ -4,4 +4,7 @@ public class github {
     public static void main(String[]args){
         System.out.println("HelloWorld");
     }
+    public static int add(int a,int b){
+        return a+b;
+    }
 }
